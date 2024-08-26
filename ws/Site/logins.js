d1e73f3306c2cc306cd7fa81d72fee87bd1b04c8
khalilopigmeu <87873745+khalilopigmeu@users.noreply.github.com>
2024-08-26 09:07:02 -0300

    Update logins.js

--- ws/Site/logins.js
+++ ws/Site/logins.js
@@ -29,10 +29,10 @@ app["LoginsOauth"] = new Vue({
                     } else {
                         setAuth(auth);
                     }
+                    app.configuracaosite.buscar(app.LoginsOauth.src[0].IdEmpresa);
                     window.localStorage.setItem("RAVEC", app.LoginsOauth.src[0].RAVEC);
                     window.localStorage.setItem("IdLogin", app.LoginsOauth.src[0]._id["$oid"]);
                     app.sys.acessar(window.localStorage.getItem("IdLogin"), window.localStorage.getItem("RAVEC"));
-                    app.configuracaosite.buscar(app.LoginsOauth.src[0].IdEmpresa);
                 } else {
                     setAuth(decrypt(app.sys.bien, "encodedstring"));
                     window.localStorage.setItem("IdLogin", id);
